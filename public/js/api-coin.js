4043b6e94a182c91b90d0e2e541b5ce89b1dfeec
orpillasam <31043722+orpillasam@users.noreply.github.com>
2018-02-09 14:49:53 -0800

    modal button

--- public/js/api-coin.js
+++ public/js/api-coin.js
@@ -225,6 +225,7 @@ $(document).ready( function() {
             //****************bitcoin**********************//
             $("#bitcoin-qty").text(BTCQuanity);
 
+
             let BTCValue = (currentBTC * BTCQuanity).toFixed(2);
             $("#bitcoin-value").text("$" + BTCValue);
             console.log("BTC value is " + BTCValue);
@@ -437,7 +438,31 @@ $(document).ready( function() {
         // changeCoin(currentPriceETC);
         // changeCoin(currentPriceBCH);
 
+// Get the modal
+var modal = document.getElementById('myModal');
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 
