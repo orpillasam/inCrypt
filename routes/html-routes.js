25b291e7650468959acdedd879954b53b3ac56e4
orpillasam <orpillasam@gmail.com>
2018-02-09 06:51:54 -0800

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,4 +10,19 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname, "../public/coins.html"));
     });
 
+    app.get("/btc-coins", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/coins.html"));
+    });
+
+    app.get("/ltc-coins", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/coins.html"));
+    });
+
+    app.get("/eth-coins", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/coins.html"));
+    });
+
+    app.get("/bch-coins", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/coins.html"));
+    });
 }
