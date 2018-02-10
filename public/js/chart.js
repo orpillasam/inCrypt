7008e46d87b4ecb59b2a734f38a678d959f36752
Lauren Spinazze <lvspinazze@Laurens-MacBook-Pro.local>
2018-02-09 23:27:22 -0800

    data array

--- public/js/chart.js
+++ public/js/chart.js
@@ -34,72 +34,69 @@ $.ajax({
 
 $(document).ready( function() {
 
-    var btcData = [["2018-02-08", 7577.1],
+    var btcData = [["2018/02/08",7577.1],
     [
-    "2018-02-07",
-    7694.96
+    1518076800000,7694.96
     ],
     [
-    "2018-02-06",
-    6882.77
+    1517990400000,6882.77
     ],
     [
-    "2018-02-05",
-    8139.14
+    1517904000000,8139.14
     ],
     [
-    "2018-02-04",
-    9078.04
+    1517817600000,9078.04
     ],
     [
-    "2018-02-03",
-    8830.89
+    1517731200000,8830.89
     ],
     [
-    "2018-02-02",
-    8954.31
+    1517644800000,8954.31
     ],
     [
-    "2018-02-01",
-    10133.97
+    1517558400000,10133.97
     ],
     [
-    "2018-01-31",
-    10012.71
+    1517472000000,10012.71
     ],
     [
-    "2018-01-30",
-    11092.54
+    1517385600000,11092.54
     ],
     [
-    "2018-01-29",
-    11665.64
+    1517299200000,11665.64
     ],
     [
-    "2018-01-28",
-    11402.73
+    1517212800000,11402.73
     ],
     [
-    "2018-01-27",
-    11222.13
+    1517126400000,11222.13
     ],
     [
-    "2018-01-26",
-    11277.27
+    1517040000000,11277.27
     ],
     [
-    "2018-01-25",
-    11336.11
+    1516953600000,11336.11
     ],
     [
-    "2018-01-24",
-    10852.99
+    1516867200000,10852.99
     ],
     [
-    "2018-01-23",
-    11125.65
+    1516780800000,11125.65
     ]]
-    
+
+    /*var btcData = [];
+
+    $.ajax({
+        url: "https://min-api.cryptocompare.com/data/pricehistorical?fsym=" + coinBTC + "&tsyms=" + conversionType + "&ts=" + moment().subtract(i, 'days').unix(),
+        method: "GET"
+    }).done(function(response){
+        console.log("url1 response is " + response);
+        currentPriceBTC = response["USD"]
+        newCurrentPriceArray.push(currentPriceBTC);
+        console.log("BTC Current Price: " + currentPriceBTC);
+        // dayChangePercent(currentPriceBTC);*/
+
+   
         var myChart = Highcharts.stockChart('btc-charts', {
     
             rangeSelector: {
@@ -112,10 +109,7 @@ $(document).ready( function() {
     
             series: [{
                 name: 'BTC',
-                data: btcData,
-                tooltip: {
-                    valueDecimals: 2
-                }
+                data: btcData
             }]
         });
     });
